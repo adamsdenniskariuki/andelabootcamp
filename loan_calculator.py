aa24922056f8acead3f74e43eb91950810e31dcf
adamsdenniskariuki <adams.kariuki1@students.ku.ac.ke>
2017-04-04 00:55:43 +0300

    last commit

--- loan_calculator.py
+++ loan_calculator.py
@@ -1,11 +1,18 @@
 # function to calculate the loan amount to be paid
-def loan_calculator(Amount, Rate, Time):
-	Rate = float(Rate) / 100
-	Time = float(Time / 12)
 
-	if(Time < 0 or Time > 1):
+def loan_calculator(Amount, Rate, Time):
+
+	if(isinstance(Amount, (int, float)) == False or isinstance(Rate, (int, float))  == False or isinstance(Time, (int, float))  == False):
+		return "invalid input"
+	elif(Amount <= 0):
+		return "invalid amount"
+	elif (Rate <= 0 or Rate > 100):
+		return "invalid rate"
+	elif(Time <= 0 or Time > 12):
 		return "invalid time"
 	else:
+		Rate = float(Rate) / 100
+		Time = float(Time / 12)
 		new_amount = float((Amount * Rate * Time) + Amount)
 		return new_amount
 
