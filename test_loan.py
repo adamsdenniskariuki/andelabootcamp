4c8119ed33e9341c1b525ebb9e504fe37e1fce9c
adamsdenniskariuki <adams.kariuki1@students.ku.ac.ke>
2017-04-04 00:52:30 +0300

    last commit

--- test_loan.py
+++ test_loan.py
@@ -1,12 +1,26 @@
 import unittest
 from loan_calculator import loan_calculator
 
+#class to hold the test cases for the loan calculator
 class LoanCalculator(unittest.TestCase):
-	#def test_accepts_integer_values(self):
-	#	self.assertEquals(loan_calculator(), )
 
+	#test to determine if the loan calculator works
 	def test_it_works(self):
-		self.assertEquals(loan_calculator(100000, 12,12), 112000)
+		self.assertEquals(loan_calculator(100000, 12, 12), 112000)
 
+	#test to ensure the user inputs are integers or decimals is validated
+	def test_inputs(self):
+		self.assertEquals(loan_calculator("12000", "12", "45"), 'invalid input')
+
+	#test to ensure the amount is validated
+	def test_amount(self):
+		self.assertEquals(loan_calculator(0, 12, 10), 'invalid amount')	
+
+	#test to ensure time is validated
 	def test_time(self):
 		self.assertEquals(loan_calculator(100000, 12, -10), 'invalid time')
+
+	#test to ensure rate is validated
+	def test_rate(self):
+		self.assertEquals(loan_calculator(100000, -18, 10), 'invalid rate')
+
