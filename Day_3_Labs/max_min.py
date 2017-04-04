b044c5562566ba66fa22bd3c7dae618f28908090
adamsdenniskariuki <adams.kariuki1@students.ku.ac.ke>
2017-04-04 15:06:08 +0300

    040420171506

---
+++ Day_3_Labs/max_min.py
@@ -0,0 +1,13 @@
+def find_max_min(l):
+	
+	l.sort()
+
+	min_num = l[0]
+	max_num = l[-1]
+
+	if(min_num == max_num):
+		 return [len(l)]
+	else:
+		return [min_num, max_num] 
+
+print find_max_min([4, 66, 6, 44, 7, 78, 8, 68, 2])
---
+++ Day_3_Labs/word_count.py
@@ -0,0 +1,17 @@
+def words(word):
+
+	list_of_words = [int(i) if i.isdigit() else i for i in word.split()]
+	list_of__unique_words = list(set(list_of_words))
+	out = {}
+
+	for u in list_of__unique_words:
+		count = 0
+		for w in list_of_words:
+			if (w == u):
+				count += 1
+		out[u] = count 
+
+	print list_of_words
+	return out
+
+print words("testing 1\t2 testing")
