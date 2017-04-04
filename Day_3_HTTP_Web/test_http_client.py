aea6915c9055c56e80e70f8903141053af69223b
adamsdenniskariuki <adams.kariuki1@students.ku.ac.ke>
2017-04-04 19:19:35 +0300

    040420171919

---
+++ Day_3_HTTP_Web/test_http_client.py
@@ -0,0 +1,10 @@
+import unittest
+from http_client import http_client
+
+class HTTPClientTest(unittest.TestCase):
+	
+	def test_it_works(self):
+		result = http_client('http://api.openweathermap.org/data/2.5/forecast/daily?q=Nairobi&APPID=9e68c31e59d752247d58d439c50aaaff')
+		out = json.load(result)
+		country = out['city']['country']
+		self.assertEqual(country, 'KE', "Not working")
