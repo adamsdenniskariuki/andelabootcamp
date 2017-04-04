5dfa701f34040feb22945e234748bf27048ee19b
adamsdenniskariuki <adams.kariuki1@students.ku.ac.ke>
2017-04-04 20:04:20 +0300

    040420172004

--- Day_3_HTTP_Web/http_client.py
+++ Day_3_HTTP_Web/http_client.py
@@ -1,23 +1,29 @@
 # http client to consume a public API
 
-import urllib2
+from urllib.request import urlopen, Request, HTTPError
+import json
 
 def http_client(url):
     
-    request = urllib2.Request(url)
+    request = Request(url)
     
     try:
-        response = urllib2.urlopen(request)
-        output = response.read()
+        response = urlopen(request)
+        output = json.loads(response.read().decode('utf-8'))
         
-    except urllib2.HTTPError as error:
+    except HTTPError as error:
         return 'Error connecting to the API:', error
         
     else:
-        return output
+        return output['city']['country']
     
 # Open Weather API
 print(http_client('http://api.openweathermap.org/data/2.5/forecast/daily?q=Nairobi&APPID=9e68c31e59d752247d58d439c50aaaff'))
 
-# NASA API
+"""
+
+Another example call to the NASA API
+
 print(http_client('https://api.nasa.gov/planetary/apod?api_key=iQ4eh40kqXhUlqeH480pMjR54TQXDY1ZU2FV7Bct'))
+
+"""
--- Day_3_HTTP_Web/test_http_client.py
+++ Day_3_HTTP_Web/test_http_client.py
@@ -1,13 +1,11 @@
 import unittest
-import urllib2
-import json
 
 from http_client import http_client
 
 class HTTPClientTest(unittest.TestCase):
 	
 	def test_it_works(self):
+
 		result = http_client('http://api.openweathermap.org/data/2.5/forecast/daily?q=Nairobi&APPID=9e68c31e59d752247d58d439c50aaaff')
-		out = json.load(result)
-		country = out['city']['country']
-		self.assertEqual(country, 'KE', "Not working")
+		
+		self.assertEqual(result, 'KE', "Not working")
