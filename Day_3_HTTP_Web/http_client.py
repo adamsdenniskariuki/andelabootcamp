733e3aabded5de27a4f2861b60fa88b134fd5444
adamsdenniskariuki <adams.kariuki1@students.ku.ac.ke>
2017-04-04 18:36:58 +0300

    040420171836

--- Day_3_HTTP_Web/http_client.py
+++ Day_3_HTTP_Web/http_client.py
@@ -8,12 +8,16 @@ def http_client(url):
     
     try:
         response = urllib2.urlopen(request)
-        weather = response.read()
+        output = response.read()
         
     except urllib2.URLError, error:
         return 'Error connecting to the weather API:', 
         
     else:
-        return weather
+        return output
     
+# Open Weather API
 print(http_client('http://api.openweathermap.org/data/2.5/forecast/daily?q=Nairobi&APPID=9e68c31e59d752247d58d439c50aaaff'))
+
+# NASA API
+print(http_client('https://api.nasa.gov/planetary/apod?api_key=iQ4eh40kqXhUlqeH480pMjR54TQXDY1ZU2FV7Bct'))
